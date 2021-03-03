fc0b059b3c7752ceaff727d6dfe0672e26fea38d
korbiwe <korbiwensky@gmail.com>
2021-03-03 15:37:31 +0300

    v1.1.1 fix time being calculated improperly

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const getHrTime = () => {
-    const [seconds, microseconds] = process.hrtime();
-    return Number(`${seconds}.${microseconds}`)
+    const nanoseconds = process.hrtime.bigint();
+    return nanoseconds / 1e6;
 };
 
 // Timer names should ideally be unique
